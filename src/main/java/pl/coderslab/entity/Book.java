9e8f45f4eb69da15b6520a2aaecd6d2756c29aaa
Troscianczyk, Marcin <marcin.troscianczyk.cl@gmail.com>
2023-06-03 13:24:30 +0200

    day 3 - validation ex. 2

--- src/main/java/pl/coderslab/entity/Book.java
+++ src/main/java/pl/coderslab/entity/Book.java
@@ -3,6 +3,7 @@ package pl.coderslab.entity;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.validator.constraints.Range;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -12,6 +13,11 @@ import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,16 +32,27 @@ public class Book {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
+    @Size(min = 5)
     private String title;
+
+    @Range(min = 1, max = 10)
     @ToString.Exclude
     private int rating;
+
+    @Size(max = 600)
     private String description;
 
+    @NotNull
     @ManyToOne//(fetch = FetchType.LAZY)//(cascade = CascadeType.PERSIST)
     private Publisher publisher;
 
+    @NotEmpty
     @ManyToMany(fetch = FetchType.EAGER)
     @ToString.Exclude
     private List<Author> authors = new ArrayList<>();
+
+    @Min(1)
+    private int pages;
 
 }
