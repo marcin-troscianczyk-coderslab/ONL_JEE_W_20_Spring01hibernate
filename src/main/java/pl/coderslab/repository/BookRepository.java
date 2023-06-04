74304e60b6f39da99f3602be4b41eaef886ee7f2
Troscianczyk, Marcin <marcin.troscianczyk.cl@gmail.com>
2023-06-04 10:39:36 +0200

    day 4 - spring data ex. 1

--- src/main/java/pl/coderslab/repository/BookRepository.java
+++ src/main/java/pl/coderslab/repository/BookRepository.java
@@ -1,6 +1,8 @@
 package pl.coderslab.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pl.coderslab.entity.Author;
 import pl.coderslab.entity.Book;
 import pl.coderslab.entity.Category;
@@ -13,11 +15,15 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 
     List<Book> findByRating(int rating);
     List<Book> findByPublisherIsNotNull();
-    List<Book> findByPublisher(Publisher publisher);
+    List<Book> findByPublisher(Publisher publisher); // select b from Book b where b.publisher = :publisher
     List<Book> findByAuthorsContains(Author author);
 
-    List<Book> findByTitle(String title);
-    List<Book> findByCategory(Category category);
+    @Query("select b from Book b where b.title = :title")
+    List<Book> findByTitle(@Param("title") String title);
+
+    @Query("select b from Book b where b.category = :category")
+    List<Book> findByCategory(@Param("category") Category category);
+
     List<Book> findByCategoryId(Long id);
-    Optional<Book> findFirstByCategoryOrderByTitle(Category category);
+    Optional<Book> findFirstByCategoryOrderByTitle(Category category); // select b from Book b where b.category = :category order by b.title asc limit 1
 }
